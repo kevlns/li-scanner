54f0f042cb3a25ce8128630e047290e1a9cddba4
cyan0123456789 <1822367636@qq.com>
2023-03-20 16:50:38 +0800

    new subject UI

--- li_scanner/cardRecognitionModule/SubjectUI.py
+++ li_scanner/cardRecognitionModule/SubjectUI.py
@@ -1,37 +1,33 @@
-# 导出成绩
-# 每题分数记到excel
 import sys
-
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QCoreApplication
 from PyQt5.QtGui import QPixmap, QDoubleValidator
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QHBoxLayout, QVBoxLayout, QLineEdit, \
-    QPushButton, QComboBox, QMessageBox
-import xlwings
+from PyQt5.QtWidgets import QApplication, QWidget, QComboBox, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit
 
 
-class ZhuguantiUI(QMainWindow):
+class SubUI(QWidget):
 
     def __init__(self, StuID: list, Numofquestion: int):
         """
-        :param StuID: 学号列表
-        :param Numofquestion: 分割出的图片个数
+            :param StuID: 学号列表
+            :param Numofquestion: 分割出的图片个数
         """
-        super(ZhuguantiUI, self).__init__()
+        super(SubUI, self).__init__()
+
         self.StuID = StuID  # 学号列表
-        self.scores = {}  # 储存每人每道题得分
-        self.index_ID = 0  # 列表索引
-        self.path = "../../record/subjective/"  # 题目文件夹存放路径
-        self.input_score = QLineEdit()
-        self.label_pic = QLabel(self)
-        self.total_num = Numofquestion
+        self.total_num = Numofquestion  # 题目数量
         self.num = 0  # 第几道大题
-        self.cb_num = QComboBox(self)  # 大题题号选择下拉框
+
+        self.scores = {}  # 储存每人每道题得分
         self.total_score = {}  # 每人大题总分
-        for id in self.StuID:
-            self.scores[id] = []
-            self.total_score[id] = 0
+
+        self.combobox_num = QComboBox(self)  # 题号选择下拉框
+
+        for Stu_id in self.StuID:
+            self.scores[Stu_id] = []
+            self.total_score[Stu_id] = 0
             for nq in range(0, Numofquestion):
-                self.scores[id].append(0)
+                self.scores[Stu_id].append(0)
+
         self.initUI()
 
     def initUI(self):
@@ -40,189 +36,190 @@ class ZhuguantiUI(QMainWindow):
         # 设置窗口标题
         self.setWindowTitle("轻阅-主观题批改")
 
-        # 设置下拉框
         for index in range(0, self.total_num):
-            self.cb_num.addItem("第" + str(index + 1) + "题")
-        self.cb_num.setStyleSheet('font-size: 40px;')
-        self.cb_num.currentIndexChanged.connect(self.SetNum)
+            self.combobox_num.addItem("第" + str(index + 1) + "题")
+        self.combobox_num.setStyleSheet('font-size: 45px;')
+        # self.combobox_num.currentIndexChanged.connect(self.SetNum)
+        hbox_win1_combobox = QHBoxLayout()
+        hbox_win1_combobox.addSpacing(60)
+        hbox_win1_combobox.addWidget(self.combobox_num)
+        hbox_win1_combobox.addSpacing(60)
 
         label_title = QLabel(self)
-        label_title.setText("主观题批改")
-        label_title.setStyleSheet('font-size: 50px; color: #24806A; font-weight: bold')
+        label_title.setText("请选择批改题目：")
+        label_title.setStyleSheet('font-size: 50px;font-weight: bold')
 
-        button_export = QPushButton("导出成绩")
+        button_yes = QPushButton("确认")
+        button_yes.setStyleSheet('font-size: 30px;')
+        button_yes.clicked.connect(self.correction)
+        button_no = QPushButton("取消")
+        button_no.setStyleSheet('font-size: 30px;')
+        button_no.clicked.connect(QCoreApplication.instance().quit)
 
-        button_start = QPushButton("进入")
-        button_start.clicked.connect(self.correction)
-
-        hbox_win1_top = QHBoxLayout()
-        hbox_win1_top.addSpacing(25)
-        hbox_win1_top.addWidget(label_title, 4)
-        hbox_win1_top.addStretch(1)
-        hbox_win1_top.addWidget(button_export, 1)
-        hbox_win1_top.addSpacing(25)
-
-        hbox_win1_middle = QHBoxLayout()
-        hbox_win1_middle.addWidget(self.cb_num, 0, Qt.AlignJustify | Qt.AlignCenter)
-
-        hbox_win1_bottom = QHBoxLayout()
-        hbox_win1_bottom.addWidget(button_start, 0, Qt.AlignRight | Qt.AlignCenter)
+        hox_win1_button = QHBoxLayout()
+        hox_win1_button.addWidget(button_yes)
+        hox_win1_button.addStretch(1)
+        hox_win1_button.addWidget(button_no)
 
         vbox_win1 = QVBoxLayout()
+        vbox_win1.addStretch(1)
+        vbox_win1.addWidget(label_title)
         vbox_win1.addSpacing(30)
-        vbox_win1.addLayout(hbox_win1_top, 1)
-        vbox_win1.addLayout(hbox_win1_middle, 3)
-        vbox_win1.addLayout(hbox_win1_bottom, 1)
-        vbox_win1.addSpacing(10)
+        vbox_win1.addLayout(hbox_win1_combobox)
+        vbox_win1.addSpacing(45)
+        vbox_win1.addLayout(hox_win1_button)
+        vbox_win1.addStretch(1)
 
-        window_1 = QWidget()
-        window_1.setLayout(vbox_win1)
-        self.setCentralWidget(window_1)
+        hbox_win1 = QHBoxLayout()
+        hbox_win1.addSpacing(100)
+        hbox_win1.addLayout(vbox_win1)
+        hbox_win1.addSpacing(100)
 
-    # def changePicture(self):
-    #     print(1)
-    #     self.label_pic.setPixmap(f"cut{self.num}.jpg")
+        self.setLayout(hbox_win1)
 
-    # 每道大题批改界面
     def correction(self):
+        self.hide()
+        self.main_cor = CorrectionUI(self.StuID, self.num, self.scores, self.total_score)
+        # print(self.StuID, self.num)
+        self.main_cor.show()
+
+    # # 读取下拉框索引，显示对应大题
+    # def SetNum(self):
+    #     self.num = self.combobox_num.currentIndex()
+    #     pixmap = QPixmap(self.path + f"{self.StuID[self.index_ID]}/{self.StuID[self.index_ID]}-{self.num}.jpg")
+    #     self.label_pic.setPixmap(pixmap)
+    #     self.label_pic.setScaledContents(True)
+
+
+class CorrectionUI(QWidget):
+
+    def __init__(self, StuID: list, SubNum: int, Scores: dict, TotalScore: dict):
+        super(CorrectionUI, self).__init__()
+
+        self.StuID = StuID  # 学号列表
+        self.sub_num = SubNum  # 题目序号
+        self.scores = Scores
+        self.total_score = TotalScore
+        self.index_ID = 0  # 列表索引
+        self.path = "../../record/subjective/"  # 题目文件夹存放路径
+        # self.scores = {}  # 储存每人每道题得分
+        # self.total_score = {}  # 每人大题总分
+
+        self.label_pic = QLabel()  # 图片显示
+        self.lineedit_score = QLineEdit()   # 分数输入框
+
+        self.initUI()
+
+    def initUI(self):
         # 设置窗口大小
         self.resize(1000, 618)
         # 设置窗口标题
-        self.setWindowTitle(f"轻阅-{self.cb_num.currentText()}批改")
-
-        # 加载图片
-        pixmap = QPixmap(self.path + f"{self.StuID[self.index_ID]}/{self.StuID[self.index_ID]}-{self.num}.jpg")
-        self.label_pic.setPixmap(pixmap)
-        self.label_pic.setScaledContents(True)
+        self.setWindowTitle(f"轻阅-第{self.sub_num + 1}题批改")
 
         # 显示题号
         label_title = QLabel(self)
-        label_title.setText(self.cb_num.currentText())
-        label_title.setStyleSheet('font-size: 35px; font-weight: bold')
+        label_title.setText(f"第{self.sub_num + 1}题：")
+        label_title.setStyleSheet('font-size: 50px;font-weight: bold')
 
-        hbox_win2_top = QHBoxLayout()
-        hbox_win2_top.addSpacing(15)
-        hbox_win2_top.addWidget(label_title, Qt.AlignLeft | Qt.AlignCenter)
+        # 加载图片
+        pixmap = QPixmap(self.path + f"{self.StuID[self.index_ID]}/{self.StuID[self.index_ID]}-{self.sub_num}.jpg")
+        self.label_pic.setPixmap(pixmap)
+        self.label_pic.setScaledContents(True)
+
+        # 得分输入
+        label_sco = QLabel("得分：")
+        label_sco.setStyleSheet('font-size: 25px;')
 
         # 创建打分及切换其他人答案水平布局
-        self.input_score.setFixedWidth(50)
-        self.input_score.setFixedHeight(40)
+        self.lineedit_score.setFixedWidth(150)
+        self.lineedit_score.setFixedHeight(40)
         # 校验器，只能输入两位整数和一位小数
         doubleValidator = QDoubleValidator(self)
         doubleValidator.setRange(0, 99)
         doubleValidator.setNotation(QDoubleValidator.StandardNotation)
         doubleValidator.setDecimals(1)
-        self.input_score.setValidator(doubleValidator)
+        self.lineedit_score.setValidator(doubleValidator)
 
-        lable_sco = QLabel("得分：")
-        lable_sco.setStyleSheet('font-size: 25px;')
+        hbox_win2_sco = QHBoxLayout()
+        hbox_win2_sco.addSpacing(20)
+        hbox_win2_sco.addWidget(label_sco, Qt.AlignLeft | Qt.AlignCenter)
+        hbox_win2_sco.addWidget(self.lineedit_score, Qt.AlignLeft | Qt.AlignCenter)
 
-        lable_midright = QLabel("分")  # lable_midright = QLabel(f"分/{该题总分}分")
-        lable_midright.setStyleSheet('font-size: 25px;')
+        button_next = QPushButton("下一张")
+        button_last = QPushButton("上一张")
+        button_next.clicked.connect(self.nextbutClicked)
+        button_last.clicked.connect(self.lastbutClicked)
 
-        hbox_win2_midright = QHBoxLayout()
-        hbox_win2_midright.addWidget(self.input_score)
-        hbox_win2_midright.addWidget(lable_midright)
+        hbox_win2_but = QHBoxLayout()
+        hbox_win2_but.addStretch(1)
+        hbox_win2_but.addWidget(button_last, 2)
+        hbox_win2_but.addStretch(2)
+        hbox_win2_but.addWidget(button_next, 2)
+        hbox_win2_but.addStretch(1)
 
-        vbox_win2_midright = QVBoxLayout()
-        vbox_win2_midright.addWidget(lable_sco)
-        vbox_win2_midright.addLayout(hbox_win2_midright)
+        vbox_win2 = QVBoxLayout()
+        vbox_win2.addSpacing(50)
+        vbox_win2.addWidget(label_title, Qt.AlignLeft | Qt.AlignCenter)
+        vbox_win2.addWidget(self.label_pic, Qt.AlignJustify | Qt.AlignCenter)
+        vbox_win2.addSpacing(15)
+        vbox_win2.addLayout(hbox_win2_sco)
+        vbox_win2.addSpacing(45)
+        vbox_win2.addLayout(hbox_win2_but)
+        vbox_win2.addSpacing(75)
 
-        # 创建图片水平布局
-        hbox_pic = QHBoxLayout()
-        hbox_pic.addSpacing(20)
-        hbox_pic.addWidget(self.label_pic)
-        hbox_pic.addStretch(1)
-        hbox_pic.addLayout(vbox_win2_midright)
-        hbox_pic.addStretch(1)
+        hbox_win2 = QHBoxLayout()
+        hbox_win2.addSpacing(125)
+        hbox_win2.addLayout(vbox_win2)
+        hbox_win2.addSpacing(125)
 
-        last_but = QPushButton("上一张")
-        next_but = QPushButton("下一张")
-        # button_return = QPushButton("返回")
-        last_but.clicked.connect(self.lastbutClicked)
-        next_but.clicked.connect(self.nextbutClicked)
-        # button_return.clicked.connect(self.returnbutClicked)
-        hbox_sco = QHBoxLayout()
-        hbox_sco.addStretch(1)
-        # hbox_sco.addWidget(button_return)
-        # hbox_sco.addSpacing(10)
-        hbox_sco.addWidget(last_but)
-        hbox_sco.addSpacing(10)
-        hbox_sco.addWidget(next_but)
-
-        vbox = QVBoxLayout()
-        vbox.addSpacing(15)
-        vbox.addLayout(hbox_win2_top)
-        vbox.addStretch(1)
-        vbox.addLayout(hbox_pic)
-        vbox.addStretch(1)
-        vbox.addLayout(hbox_sco)
-        vbox.addSpacing(15)
-
-        mainFrame = QWidget()
-        mainFrame.setLayout(vbox)
-        self.setCentralWidget(mainFrame)
-
-    # 读取下拉框索引，显示对应大题
-    def SetNum(self):
-        self.num = self.cb_num.currentIndex()
-        pixmap = QPixmap(self.path + f"{self.StuID[self.index_ID]}/{self.StuID[self.index_ID]}-{self.num}.jpg")
-        self.label_pic.setPixmap(pixmap)
-        self.label_pic.setScaledContents(True)
+        self.setLayout(hbox_win2)
 
     def lastbutClicked(self):
+        if self.lineedit_score.text() == '':
+            self.lineedit_score.setText('0')
+        # score[学号列表[学号索引]][大题索引] = 所输分数
+        self.scores[self.StuID[self.index_ID]][self.sub_num] = float(self.lineedit_score.text())
+
         self.index_ID -= 1
-        self.total_score[self.StuID[self.index_ID]] -= self.scores[self.StuID[self.index_ID]][self.num]
+        self.total_score[self.StuID[self.index_ID]] -= self.scores[self.StuID[self.index_ID]][self.sub_num]
         if self.index_ID < 0:
             self.index_ID = 0
         else:
-            self.input_score.setText('')  # 清空输入框
-        pixmap = QPixmap(self.path + f"{self.StuID[self.index_ID]}/{self.StuID[self.index_ID]}-{self.num}.jpg")
+            self.lineedit_score.setText(str(self.scores[self.StuID[self.index_ID]][self.sub_num]))  # 显示分数
+        pixmap = QPixmap(self.path + f"{self.StuID[self.index_ID]}/{self.StuID[self.index_ID]}-{self.sub_num}.jpg")
         self.label_pic.setPixmap(pixmap)
         self.label_pic.setScaledContents(True)
 
     def nextbutClicked(self):
+        if self.lineedit_score.text() == '':
+            self.lineedit_score.setText('0')
         # score[学号列表[学号索引]][大题索引] = 所输分数
-        # if self.input_score.text() is None:
-        #     print(123)
-        # else:
-        if self.input_score.text() == '':
-            self.input_score.setText('0')
-        self.scores[self.StuID[self.index_ID]][self.num] = float(self.input_score.text())
+        self.scores[self.StuID[self.index_ID]][self.sub_num] = float(self.lineedit_score.text())
         # total_score[学号列表[学号索引]]
-        self.total_score[self.StuID[self.index_ID]] += float(self.input_score.text())
+        self.total_score[self.StuID[self.index_ID]] += float(self.lineedit_score.text())
         # 点击了下一个，则学号索引 + 1
         self.index_ID += 1
         # 若已经是最后一张，则无变化
         if self.index_ID >= len(self.StuID):
             self.index_ID = len(self.StuID) - 1
         else:
-            self.input_score.clear()  # 清空输入框
-        pixmap = QPixmap(self.path + f"{self.StuID[self.index_ID]}/{self.StuID[self.index_ID]}-{self.num}.jpg")
+            self.lineedit_score.clear()  # 清空输入框
+
+        pixmap = QPixmap(self.path + f"{self.StuID[self.index_ID]}/{self.StuID[self.index_ID]}-{self.sub_num}.jpg")
         self.label_pic.setPixmap(pixmap)
         self.label_pic.setScaledContents(True)
-        # print(111)
 
-    # def returnbutClicked(self):
-    #     mbox = QMessageBox()  # QMessageBox.question(self, title, text, button, QMessageBox.No)
-    #     mbox.setWindowTitle('返回确认')
-    #     mbox.setText('是否返回题目选择界面？')
-    #     mbox.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-    #     but_y = mbox.button(QMessageBox.Yes)
-    #     but_y.setText('确认')
-    #     but_n = mbox.button(QMessageBox.No)
-    #     but_n.setText('取消')
-    #     mbox.exec_()
-    #     if mbox.clickedButton() == but_y:
-    #         pass
-    #     else:
-    #         pass
+        if self.scores[self.StuID[self.index_ID]][self.sub_num] != 0:   # 若成绩不为零，显示成绩
+            self.lineedit_score.setText(str(self.scores[self.StuID[self.index_ID]][self.sub_num]))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    main = ZhuguantiUI([202240090, 202243050, 202243051], 2)
+    main = SubUI([202240090, 202243050, 202243051], 2)
+    # main = CorrectionUI([202240090, 202243050, 202243051], 0,
+    #                     {202240090: [0, 0], 202243050: [0, 0], 202243051: [0, 0]},
+    #                     {202240090: 0, 202243050: 0, 202243051: 0})
     main.show()
 
     sys.exit(app.exec_())
